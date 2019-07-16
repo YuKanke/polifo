495e35c31cad05e54402734e169ce1057251c702
root <root@84e235d6a9c6>
2019-07-16 08:35:48 +0000

    table migration(user,portfolio) upload file(html/css/js/images)

--- app/User.php
+++ app/User.php
@@ -35,5 +35,12 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-    ];
+];
+
+
+
+    public function Portfolios(){
+	    return $this->hasMany('App\Portfolio');
+    }
+
 }
